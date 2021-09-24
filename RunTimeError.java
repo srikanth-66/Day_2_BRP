f05eaafc28f8a6e48a5ec9b95026e0ded2324f82
Srikanth_M <srikanthMidathapalli@gmail.com>
2021-09-24 08:45:47 +0530

    commiting the Run time error program

---
+++ RunTimeError.java
@@ -0,0 +1,16 @@
+package com.homeprograms;
+
+public class RunTimeError 
+{
+	/*public void add()
+	{
+		
+	}*/
+	public static void main(String[] args) {
+		RunTimeError ad=new RunTimeError();
+		ad.add();
+
+		
+	}
+
+}
