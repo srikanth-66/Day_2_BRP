6ab42a12a689a63d090b33c2f1494d6cf830225a
Srikanth_M <srikanthMidathapalli@gmail.com>
2021-09-24 08:46:55 +0530

    commiting the Sum of two dice program

---
+++ SumOFTwoDice.java
@@ -0,0 +1,15 @@
+package com.homeprograms;
+
+public class SumOFTwoDice {
+	public static void main(String[] args) {
+	    {
+	            int SIDES = 6;
+	            int a = 1 + (int) (Math.random() * SIDES);
+	            int b = 1 + (int) (Math.random() * SIDES);
+	            int sum = a + b;
+	            System.out.println(sum);
+	        }
+
+	}
+
+}
